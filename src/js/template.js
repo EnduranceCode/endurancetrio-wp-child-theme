57c34744f6d8098f014527ecfe9c315949005215
Ricardo do Canto <endurancecode@gmail.com>
2024-05-04 14:19:50 +0100

    Use side effect import for components

--- src/js/template.js
+++ src/js/template.js
@@ -23,5 +23,5 @@ import '../scss/template.scss';
 
 import './bulma/tabs';
 
-import { LiveResults } from './submodules/endurancetrio-web-components/src/app/components/live-results/LiveResults';
-import { EventResults } from './submodules/endurancetrio-web-components/src/app/components/event-results/EventResults';
+import './submodules/endurancetrio-web-components/src/app/components/live-results/LiveResults';
+import './submodules/endurancetrio-web-components/src/app/components/event-results/EventResults';
